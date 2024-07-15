ffd39302b149b8b8a6e83bcb0cdb2fd63f2fc815
ombhojane <ombhojane05@gmail.com>
2024-07-15 14:51:25 +0530

    vercel updated 8

--- app.py
+++ app.py
@@ -216,5 +216,3 @@ async def download_pdf(idea_id: str):
         )
     raise HTTPException(status_code=404, detail="Idea not found")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
